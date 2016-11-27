160de5cd31cda9f5eea657afa2b6bd7e9e8d66d0
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2016-11-27 01:06:33 -0800

    Somewhat functional? 👍

--- libhrc.lisp
+++ libhrc.lisp
@@ -2,19 +2,12 @@
 
 #! /usr/local/bin/clisp
 
-(defun suffixarray (str)
-    (defparameter i 0)
-    (defparameter len (length str))
-    (defparameter suffixArray ())
-    (loop while (< i len)
-        do (cond
-               ((and (char-equal (char str i) #\Space)
-                     (< (1+ i) len))
-                (setq suffixArray (append suffixArray (list (symbol-value 'i))))
-               ))
-        (setq i (1+ i)))
-    suffixArray
+(defun suffixarray (start str indices) 
+    (setq pos (position #\Space str :start start))
+    (if pos
+        (suffixarray (1+ pos) str (append indices (list pos)))
+        indices)
 )
 
 (setq s (read-line))
-(print (suffixarray s))
+(print (suffixarray 0 s ()))
