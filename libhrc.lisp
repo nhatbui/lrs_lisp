07c7649b4bdd47070c96be9d62dbb244703ef1a2
Nhat Bui <nhatbui@Nhats-MBP.fios-router.home>
2016-11-27 21:58:36 -0800

    Returns longest repeated consecutive substring.

--- libhrc.lisp
+++ libhrc.lisp
@@ -9,10 +9,12 @@
         indices)
 )
 
-(defun findlcp (s1 s2 bestlcp)
+(defun findlcp (i1 i2 doc bestlcp)
     ;; Find new longest common prefix if larger than bestlcp
+    (setq s1 (substring doc i1))
+    (setq s2 (substring doc i2))
     (setq len (string< s1 s2))
-    (if (> len (length bestlcp))
+    (if (and (> len (length bestlcp)) (= (+ i2 len) (1- i1)))
         (substring s1 0 len)
         bestlcp)
 )
@@ -21,9 +23,7 @@
     ;; Find the longest repeating substring for this suffix array
     (setq s1start (nth (1- index) suffixes))
     (setq s2start (nth index suffixes))
-    (setq s1 (substring doc s1start))
-    (setq s2 (substring doc s2start))
-    (setq lrs (findlcp s1 s2 lrs))
+    (setq lrs (findlcp s1start s2start doc lrs))
     (if (>= (1+ index) (list-length suffixes))
         lrs
         (findlrs (1+ index) suffixes doc lrs))
